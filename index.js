fc87a07ef05b4fabf741300c78cd2f2b3c04e8f1
Celso Miranda <contacto@celsomiranda.net>
2015-03-12 04:17:29 +0000

    new version 2

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-var stylus = require('stylus');
-var nib = require('nib');
-var axis = require('axis');
-var rupture = require('rupture');
-var jeet = require('jeet');
-var autoprefixer = require('autoprefixer-stylus');
+var renderer = require('./lib/renderer');
 
-var yasr_renderer = function(data, options, callback){
-  var yasr_config = hexo.config.yasr || {};
-
-  stylus(data.text)
-    .use(nib())
-    .use(axis())
-    .use(rupture())
-    .use(jeet())
-    .use(autoprefixer({ browsers: ['last 2 version'] }))
-    .set('filename', data.path)
-    .set('sourcemap', options)
-    .set('compress', yasr_config.compress)
-    .set('include css', true)
-    .render(callback);
-};
-
-hexo.extend.renderer.register('styl', 'css', yasr_renderer);
-hexo.extend.renderer.register('stylus', 'css', yasr_renderer);
+hexo.extend.renderer.register('styl', 'css', renderer);
+hexo.extend.renderer.register('stylus', 'css', renderer);
